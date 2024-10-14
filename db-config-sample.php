d696695701830342cd02fdedb4eb4521caa645fd
Momano <momope311@gmail.com>
2024-10-14 22:30:01 +0200

    pocetak

---
+++ db-config-sample.php
@@ -0,0 +1,20 @@
+<?php
+if(!defined('PROTECT')){die('Protected Content!');}
+
+class DBconfig {
+
+    private static $db_config = [
+        
+        'hostname'		=> '',
+        'username'      => '',
+        'password'      => '',
+        'database'		=> ''
+    ];
+
+    protected static function /*GetData*/($key) {
+        
+        // Get data
+    }
+}
+
+?>
